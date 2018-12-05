3ba2527aa4b382b9978869a6aad4931d8c57e993
MarkIsNotTaken <mdemesillo@gmail.com>
2018-12-05 10:57:21 -1000

    first

--- exercises.js
+++ exercises.js
@@ -2,6 +2,16 @@ let colors = ["red", "orange", "yellow", "green", "blue"];
 console.log(colors[0]);
 console.log(colors[2]);
 
-colors.pop(); 
 colors[4] = "ultraviolet";
-console.log(colors[4]);
+var fourthColor = colors[4];
+colors[5] = "purple";
+
+colors.unshift("black");
+
+console.log(colors.length);
+colors.pop();
+console.log(colors.length);
+
+for (i = 0; i < colors.length; i++) {
+  text
+}
